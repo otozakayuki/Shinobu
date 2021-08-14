d9268780b6a549bbd629ad29c3e2e2c4a58abf80
Yuki <74022012+otozakayuki@users.noreply.github.com>
2021-08-14 18:26:01 +0800

    Create hina.js

---
+++ command/message/hina.js
@@ -0,0 +1,15 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'hana',
+	run: async (client, message) => {
+		const embed = new MessageEmbed()
+			.setColor('RANDOM')
+			.setTitle('hana')
+			.setImage(
+				'https://i.pinimg.com/originals/a7/53/5b/a7535bd81b0299dfaf11357adfefd0d4.gif'
+			);
+
+		message.channel.send(embed);
+	}
+};
